70f34e40142b918b22e5028296348e5307a00600
Vikram-Kumar-Sahu <vikramkumarsahu989@gmail.com>
2025-07-31 10:51:39 +0530

    daa lab 2

--- Semister-5/DAA Lab/Lab 1/prefixSum.c
+++ Semister-5/DAA Lab/Lab 1/prefixSum.c
@@ -1,21 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     int n;
     printf("Enter the size of array : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
     printf("Enter the array elements");
-    for (int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         int a;
-        scanf("%d",&a);
-        arr[i]=a;
+        scanf("%d", &a);
+        arr[i] = a;
     }
-    for (int i=1;i<n;i++){
-        arr[i]=arr[i]+arr[i-1];
+    for (int i = 1; i < n; i++)
+    {
+        arr[i] = arr[i] + arr[i - 1];
     }
     printf("Final value of the Array is");
-    for (int i=0;i<n;i++){
-        printf("%d ,",arr[i]);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ,", arr[i]);
     }
 }
